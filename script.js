aa3d5b8802465dd179226dd566c7c954585265cd
adrianportela970 <adrianportela970@gmail.com>
2025-04-29 19:04:56 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const script_do_google = 'https://script.google.com/macros/s/AKfycbxAPCnxz2FS7qeX3Z3taGZ2wbKEvPU_evwxUXbQNGJdcn-qbQDZwT3h4kX0TCOTmHuy/exec';
+const script_do_google = 'https://script.google.com/macros/s/AKfycbxAPCnxz2FS7qeX3Z3taGZ2wbKEvPU_evwxUXbQNGJdcn-qbQDZwT3h4kX0TCOTmHuy/exec
+';
 const dados_do_formulario = document.forms['formulario-contato'];
 
 function enviarDados(event) {
